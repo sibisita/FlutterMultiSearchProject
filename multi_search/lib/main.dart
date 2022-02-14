91a9c8ef564889486545098a3cfd5680eec3f5fa
Sibi C B <sitasibi@gmail.com>
2022-02-14 20:53:58 +0530

    Error in previous commit rectified

--- multi_search/lib/main.dart
+++ multi_search/lib/main.dart
@@ -9,16 +9,7 @@ import 'frontend_elements/search_value_acceptor.dart';
 import 'frontend_elements/use_subdirectory_check_box.dart';
 
 void main() {
-  runApp(
-    /// Providers are above [MyApp] instead of inside it, so that tests
-    /// can use [MyApp] while mocking the providers
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => MultiSearchProvider()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 /// Makes `Counter` readable inside the devtools by listing all of its properties
@@ -46,66 +37,61 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int counter = Provider.of(context);
     return Scaffold(
-        floatingActionButton: FloatingActionButton(onPressed: () {
-          print("Hello World!");
-        }),
         body: Container(
-          color: Colors.blue,
-          child: Row(children: [
-            (const Expanded(
-                flex: 7,
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: SearchValueAcceptor(),
-                ))),
-            Expanded(
-              flex: 6,
-              child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.orangeAccent,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(12.0),
-                      ),
-                    ),
-                    child: Column(
+      color: Colors.blue,
+      child: Row(children: [
+        (const Expanded(
+            flex: 7,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: SearchValueAcceptor(),
+            ))),
+        Expanded(
+          flex: 6,
+          child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.orangeAccent,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12.0),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        Text("Hello"),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: InkWell(
-                                  hoverColor: Colors.orangeAccent,
-                                  onTap: () {
-                                    Navigator.of(context).pushNamed('/about');
-                                  },
-                                  child: const Icon(Icons.info)),
-                            )
-                          ],
-                        ),
-                        const Spacer(),
-                        const FolderPicker(
-                          icon: Icons.folder,
-                          buttonText: "Pick the folder to search.",
-                        ),
-                        const UseSubdirectoryCheckBox(),
-                        const Spacer(),
-                        const FolderPicker(
-                          icon: Icons.save,
-                          buttonText: "Pick the folder to save the results.",
-                        ),
-                        const Spacer(),
-                        const SearchButton()
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: InkWell(
+                              hoverColor: Colors.orangeAccent,
+                              onTap: () {
+                                Navigator.of(context).pushNamed('/about');
+                              },
+                              child: const Icon(Icons.info)),
+                        )
                       ],
                     ),
-                  )),
-            )
-          ]),
-        ));
+                    const Spacer(),
+                    const FolderPicker(
+                      icon: Icons.folder,
+                      buttonText: "Pick the folder to search.",
+                    ),
+                    const UseSubdirectoryCheckBox(),
+                    const Spacer(),
+                    const FolderPicker(
+                      icon: Icons.save,
+                      buttonText: "Pick the folder to save the results.",
+                    ),
+                    const Spacer(),
+                    const SearchButton()
+                  ],
+                ),
+              )),
+        )
+      ]),
+    ));
   }
 }
